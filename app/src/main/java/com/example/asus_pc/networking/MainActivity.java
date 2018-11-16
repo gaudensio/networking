4dae2b98b202387df1769bbd301c5431fcc6275f
Gaudensio <gaudensiod@gmail.com>
2018-11-16 09:19:14 +0700

    praktikum 11

---
+++ app/src/main/java/com/example/asus_pc/networking/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.asus_pc.networking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.asus_pc.networking.Adapter.MyAdapter;
+import com.example.asus_pc.networking.Model.GetPembelian;
+import com.example.asus_pc.networking.Model.Pembelian;
+import com.example.asus_pc.networking.Rest.ApiClient;
+import com.example.asus_pc.networking.Rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btGet;
+    ApiInterface mApiInterface;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btGet = (Button) findViewById(R.id.btGet);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mApiInterface  = ApiClient.getClient().create(ApiInterface.class);
+
+        btGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<GetPembelian> pembelianCall = mApiInterface.getPembelian();
+                pembelianCall.enqueue(new Callback<GetPembelian>() {
+                    @Override
+                    public void onResponse(Call<GetPembelian> call, Response<GetPembelian> response) {
+                        List<Pembelian> pembelianList = response.body().getListDataPembelian();
+                        Log.d("Retrofit Get", "Jumlah data pembelian: " + String.valueOf(pembelianList.size()));
+
+                        mAdapter = new MyAdapter(pembelianList);
+                        mRecyclerView.setAdapter(mAdapter);
+                    }
+
+                    @Override
+                    public void onFailure(Call<GetPembelian> call, Throwable t) {
+                        // Log error
+                        Log.e("Retrofit Get", t.toString());
+                    }
+                });
+            }
+        });
+
+    }
+}
